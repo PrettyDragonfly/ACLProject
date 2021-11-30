e3cb6eeba6f31a447004fb7a127eb73aca8c58f2
Camille1905 <bourcamille00@gmail.com>
2021-11-30 12:18:48 +0100

    add Ennemy

---
+++ src/model/entities/Ennemy.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Camille on 09/11/2021.
+//
+#include "Enemy.h"
+
+Enemy::Enemy(pair<int, int> _pos, int _ptVie, Map* map) : Entity(_pos, _ptVie, map) {}
---
+++ src/model/entities/Ennemy.h
@@ -0,0 +1,17 @@
+//
+// Created by Camille on 09/11/2021.
+//
+#ifndef PROJETACL_ENEMY_H
+#define PROJETACL_ENEMY_H
+
+#include "Entity.h"
+#include <utility>
+
+class Enemy : Entity{
+public:
+    Enemy(pair<int, int> _pos, int _ptVie, Map* map);
+    /**Fonction that follows the player**/
+    Follow(Player* player);
+};
+
+#endif //PROJETACL_ENEMY_H
